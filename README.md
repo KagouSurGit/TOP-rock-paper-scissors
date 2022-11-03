7d9c29cdcaad01edebf60b153d4ede2b6d86fddd
Diabozinho <kagoulecodeur@gmail.com>
2022-11-03 18:21:25 +0000

    .

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 19/10
 / link a javascript file to the html page
 / Create a function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’ in javascript file. 
+
+(holidays)
+
+03/11 
+/ Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+/ Had help from forums
--- externalFile.js
+++ externalFile.js
@@ -8,3 +8,23 @@ function getComputerChoice () {
     }
 }
 console.log(getComputerChoice());
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "No one wins, it's a tie";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win, the machine loses";
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win, the machine loses";
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You win, the machine loses";
+    } else {
+        return "You have lost against the machine, Skynet will rise";
+    }
+}
+/* const playerSelection = prompt("Start the game by picking among 'Rock, Paper, Scissors'");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection)); */
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
